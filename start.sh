1f8d95fae8f555483c8c3c3cf97afcbdcdf982b1
林金龙 <113858970@qq.com>
2020-04-01 15:41:25 +0800

    xx

---
+++ start.sh
@@ -0,0 +1,18 @@
+cnpm i&&
+npm run build&&
+rm -rf ./target&&
+mkdir ./target&&
+cp ./nuxt.config.js ./target&&
+cp ./package.json ./target&&
+cp -a ./build ./target&&
+cp ./target /root/project/jenkinsVuePm2&&
+cd /root/project/jenkinsVuePm2/target&&
+cnpm i&&
+# pm2 启动时读取的配置文件
+##configFile="pm2.config.json"&&
+# 先杀掉所有 pm2 启动的项目
+pm2 kill&&
+# 跟据指定的配置文件信息，重新启动 pm2
+##pm2 startOrGracefulReload $configFile&&
+# reset all metadata
+ pm2 start npm --name ‘jenkinsVuePm2’ -- start
